68334d3a2c6e42979b8e6ca5e5a9f5ddca170cad
erin dougher <ejdougher@gmail.com>
2020-11-17 07:51:42 +0000

    Automatically backed up by Learn

--- lib/catch_phrases.rb
+++ lib/catch_phrases.rb
@@ -9,3 +9,14 @@ mario
 def toadstool
   puts status
 end
+
+def all_phrases
+  phrase1 = "It's-a me, Mario!"
+  phrase2 = "Thank You Mario! But Our Princess Is In Another Castle!"
+  phrase3 = "It's Dangerous To Go Alone! Take This"
+
+  puts phrase1, phrase2, phrase3
+
+end
+
+all_phrases
